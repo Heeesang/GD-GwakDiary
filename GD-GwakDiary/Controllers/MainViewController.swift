5814e5ad3b6c45381b80ec403329c1d810da5e70
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-07-05 12:11:20 +0900

    create project

---
+++ GD-GwakDiary/Controllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  GD-GwakDiary
+//
+//  Created by 곽희상 on 2022/07/04.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MainViewController: UIViewController{
+    
+    private let diaryTableView: UITableView()
+    
+}
